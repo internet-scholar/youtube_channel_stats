bbe04ab2cb95a4766d2bb958c25c93d44a639129
Alexandre Goncalves <alexandre.reportagem@gmail.com>
2020-02-07 08:49:48 -0500

    fix sleep error

--- youtube_channel_stats.py
+++ youtube_channel_stats.py
@@ -127,7 +127,7 @@ class YoutubeChannelStats:
                                 connection_reset_by_peer = connection_reset_by_peer + 1
                                 logging.info("Connection reset by peer! {}".format(connection_reset_by_peer))
                                 if connection_reset_by_peer <= 10:
-                                    time.sleep(secs=self.WAIT_WHEN_CONNECTION_RESET_BY_PEER)
+                                    time.sleep(self.WAIT_WHEN_CONNECTION_RESET_BY_PEER)
                                     youtube = googleapiclient.discovery.build(serviceName="youtube",
                                                                               version="v3",
                                                                               developerKey=
